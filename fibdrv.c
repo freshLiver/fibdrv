41c968194de89eef777d56e26bed22d019a05364
freshLiver <freshliver.cys@gmail.com>
2022-03-12 16:15:46 +0800

    Fix VLA not allowed in kernel

--- fibdrv.c
+++ fibdrv.c
@@ -26,17 +26,14 @@ static DEFINE_MUTEX(fib_mutex);
 
 static long long fib_sequence(long long k)
 {
-    /* FIXME: C99 variable-length array (VLA) is not allowed in Linux kernel. */
-    long long f[k + 2];
+    long long f[2] = {0, 1};
 
-    f[0] = 0;
-    f[1] = 1;
-
-    for (int i = 2; i <= k; i++) {
-        f[i] = f[i - 1] + f[i - 2];
+    for (int i = 1; i <= k; i++) {
+        swap(f[0], f[1]);
+        f[0] += f[1];
     }
 
-    return f[k];
+    return f[0];
 }
 
 static int fib_open(struct inode *inode, struct file *file)
